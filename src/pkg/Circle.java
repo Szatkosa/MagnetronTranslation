1bfe7695dc866e04a4529f967b56ac25c14ca3d9
szatkosa <szatkosa@rose-hulman.edu>
2014-06-19 10:22:16 -0400

    Added main and circle

--- src/pkg/Circle.java
+++ src/pkg/Circle.java
@@ -1,4 +1,54 @@
 package pkg;
 
 public class Circle {
+	private double rad, inc, height;
+	
+	public Circle(double r, double height, double inc) {
+		// TODO Auto-generated constructor stub
+		this.setRad(r);
+		this.setInc(inc);
+		this.setHeight(height);
+	}
+
+	/**
+	 * @return the rad
+	 */
+	public double getRad() {
+		return rad;
+	}
+
+	/**
+	 * @param rad the rad to set
+	 */
+	public void setRad(double rad) {
+		this.rad = rad;
+	}
+
+	/**
+	 * @return the inc
+	 */
+	public double getInc() {
+		return inc;
+	}
+
+	/**
+	 * @param inc the inc to set
+	 */
+	public void setInc(double inc) {
+		this.inc = inc;
+	}
+
+	/**
+	 * @return the height
+	 */
+	public double getHeight() {
+		return height;
+	}
+
+	/**
+	 * @param height the height to set
+	 */
+	public void setHeight(double height) {
+		this.height = height;
+	}
 }
---
+++ src/pkg/Main.java
@@ -0,0 +1,13 @@
+package pkg;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double r = 5;
+		double height = 2;
+		double inc = 3;
+		Circle c = new Circle(r, height, inc)
+	}
+
+}
