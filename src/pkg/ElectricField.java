da3db700498125eb1d20310f8bc477c4d40b971a
szatkosa <szatkosa@rose-hulman.edu>
2014-06-19 14:12:57 -0400

    Reformatted

--- src/pkg/ElectricField.java
+++ src/pkg/ElectricField.java
@@ -8,11 +8,11 @@ public class ElectricField {
 	private static final boolean DEFAULT_WAVE = false;
 	private static final double DEFAULT_BMAG = 0;
 	private static final double DEFAULT_NE = 0;
-	
+
 	private static final double RS = 2.5E-3;
 	private static final double Q = 1.602E-19;
 	private static final double EPSILON_0 = 8.854E-12;
-	private static final double K = 2*Math.PI/(0.005);
+	private static final double K = 2 * Math.PI / (0.005);
 	private double z;
 	private double time;
 	private double e0;
@@ -27,12 +27,13 @@ public class ElectricField {
 	public ElectricField(double z, double time, double e0, boolean wave,
 			double bmag, double ne) {
 		// TODO Auto-generated constructor stub
-		this.recalculate(z,time,e0,wave,bmag,ne);
+		this.recalculate(z, time, e0, wave, bmag, ne);
 	}
 
 	public ElectricField() {
 		// TODO Auto-generated constructor stub
-		this.recalculate(DEFAULT_Z, DEFAULT_TIME, DEFAULT_E0, DEFAULT_WAVE, DEFAULT_BMAG, DEFAULT_NE);
+		this.recalculate(DEFAULT_Z, DEFAULT_TIME, DEFAULT_E0, DEFAULT_WAVE,
+				DEFAULT_BMAG, DEFAULT_NE);
 	}
 
 	public void recalculate(double z, double time, double e0, boolean wave,
@@ -50,22 +51,27 @@ public class ElectricField {
 	public double recalculate() {
 		// TODO Auto-generated method stub
 		this.r = z - 0.03;
-		this.omegape = Math.sqrt((Math.pow(ElectricField.Q, 2))*this.ne/(this.EPSILON_0));
-		this.omegace = (ElectricField.Q*this.bmag / Electron.ELECTRON_MASS);
-		this.omegauh = Math.sqrt(Math.pow(omegape, 2)+Math.pow(omegace, 2));
+		this.omegape = Math.sqrt((Math.pow(ElectricField.Q, 2)) * this.ne
+				/ (this.EPSILON_0));
+		this.omegace = (ElectricField.Q * this.bmag / Electron.ELECTRON_MASS);
+		this.omegauh = Math.sqrt(Math.pow(omegape, 2) + Math.pow(omegace, 2));
 		double ef = (Double) null;
-		
-		if (r > ElectricField.RS){
-			if (this.wave){
-				ef = -71.42857142 + this.e0*Math.sin(ElectricField.K*this.z-this.omegauh*time);
+
+		if (r > ElectricField.RS) {
+			if (this.wave) {
+				ef = -71.42857142
+						+ this.e0
+						* Math.sin(ElectricField.K * this.z - this.omegauh
+								* time);
 			} else {
 				ef = -71.42857142 + this.e0;
 			}
 		} else {
-			ef = -5.983571430E5 + 4.786285714E8*(r) - 9.572571429E10*Math.pow(r,2);
+			ef = -5.983571430E5 + 4.786285714E8 * (r) - 9.572571429E10
+					* Math.pow(r, 2);
 		}
-		
+
 		return ef;
 	}
-	
+
 }
--- src/pkg/Main.java
+++ src/pkg/Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class Main {
 	public static ArrayList<Electron> electrons = new ArrayList<Electron>();
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		double z = 0;
@@ -12,22 +13,22 @@ public class Main {
 		boolean wave = true;
 		double Bmag = 0;
 		double ne = 0;
-		ElectricField ef = new ElectricField(z,time,E0,wave,Bmag,ne);
+		ElectricField ef = new ElectricField(z, time, E0, wave, Bmag, ne);
 		ElectricField ef2 = new ElectricField();
-		ef2.recalculate(z,time,E0,wave,Bmag,ne);
+		ef2.recalculate(z, time, E0, wave, Bmag, ne);
 		ef2.recalculate();
 	}
 
 	private static void convertTrajectoriestoDensity(int t) {
 		// TODO Complete Later
 	}
-	
-	private static void createOrderedElectronDensity(double N, double[] bounds){		
-		for (int i = 0; i<N; i++){
-			for (int k = 0; k < N; k++){
-				double R = bounds[0]/(N+1);
-				double[] pos = {R,0,Electron.ELECTRON_STARTING_HEIGHT};
-				double[] velocity = {0,0,0};
+
+	private static void createOrderedElectronDensity(double N, double[] bounds) {
+		for (int i = 0; i < N; i++) {
+			for (int k = 0; k < N; k++) {
+				double R = bounds[0] / (N + 1);
+				double[] pos = { R, 0, Electron.ELECTRON_STARTING_HEIGHT };
+				double[] velocity = { 0, 0, 0 };
 				Main.electrons.add(new Electron(pos, velocity, bounds));
 			}
 		}
